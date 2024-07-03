af16f18011a8633cd38b2c0d181ee783212c503f
bluejutzu <kalebdaniel98@gmail.com>
2024-07-03 18:12:41 +0200

    disabled cors for my own sanity

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const corsOptions = {
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use("/", baseMiddleware);
 app.use("/", baseRouter);
