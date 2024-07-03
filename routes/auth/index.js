73fed57c25be8d31bc2bb9a73df5b1f70c007b67
bluejutzu <kalebdaniel98@gmail.com>
2024-07-03 15:20:43 +0200

    domain

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -97,6 +97,7 @@ router.get("/callback", async (req, res) => {
   res
     .status(200)
     .cookie("token", token, {
+      domain: "ticketit.vercel.app",
       path: "/",
       httpOnly: true,
       secure: process.env.NODE_ENV === "development",
